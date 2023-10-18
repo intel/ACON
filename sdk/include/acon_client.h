4b79bf8169de5e6507fb043f82f0851e06aaf1b1
Xiangquan Liu <xiangquan.liu@intel.com>
2023-10-18 21:11:34 +0800

    Fix review comments
    Signed-off-by: Xiangquan Liu <xiangquan.liu@intel.com>

--- sdk/include/acon_client.h
+++ sdk/include/acon_client.h
@@ -3,42 +3,43 @@
 
 #ifndef _ACON_CLIENT_H_
 #define _ACON_CLIENT_H_
+#include <stdint.h>
 
 #define SOCKET_PATH "/shared/acon.sock"
 
 typedef struct {
-    char type;
-    char subtype;
-    char version;
-    char _reserved;
+    uint8_t type;
+    uint8_t subtype;
+    uint8_t version;
+    uint8_t _reserved;
 } report_type_t;
 
 typedef struct {
-    char cpu_svn[16];
+    uint8_t cpu_svn[16];
 } cpu_svn_t;
 
 typedef struct {
-    char hash[48];
+    uint8_t hash[48];
 } sha384_hash_t;
 
 typedef struct {
     report_type_t report_type;
-    char reserved[12];
+    uint8_t reserved[12];
     cpu_svn_t cpu_svn;
     sha384_hash_t tee_tcb_info_hash;
     sha384_hash_t tee_info_hash;
-    char report_data[64];
-    char _reserved[32];
-    char mac[32];
+    uint8_t report_data[64];
+    uint8_t _reserved[32];
+    uint8_t mac[32];
 } report_mac_t;
 
 typedef struct {
-    char hash[239];
+    uint8_t hash[239];
 } tee_tcb_info_t;
 
 typedef struct {
-    char attributes[8];
-    char xfam[8];
+    uint8_t attributes[8];
+    uint8_t xfam[8];
     sha384_hash_t mrtd;
     sha384_hash_t mr_config_id;
     sha384_hash_t mr_owner;
@@ -47,44 +48,43 @@ typedef struct {
     sha384_hash_t rtmr1;
     sha384_hash_t rtmr2;
     sha384_hash_t rtmr3;
-    char _reserved[112];
+    uint8_t _reserved[112];
 } td_info_t;
 
 typedef struct {
     report_mac_t report_mac;
     tee_tcb_info_t tee_tcb_info;
-    char _reserved[17];
+    uint8_t _reserved[17];
     td_info_t td_info;
 } td_report_t;
 
 typedef struct {
-    int command;    // even = request; odd = response; negative = error
-    int size;       // size of the whole request/response
+    int32_t command;     // even = request; odd = response; negative = error
+    uint32_t size;       // size of the whole request/response
 } acon_message_hdr_t;
 
 typedef struct {
     acon_message_hdr_t header;  // command = -Exxx, as defintion in Linux
-    int request;                // original request code
+    int32_t request;            // original request code
 } acon_message_err_t;
 
 typedef struct {
     acon_message_hdr_t header;  // command = 0
-    bool is_quote;
-    long long nonce[2];
-    int data_type;              // 0 = no data; 1 = binary; 2 = string; others = reserved
+    uint32_t data_type;
+    uint64_t nonce[2];
+    int32_t attest_data_type;   // 0 = no data; 1 = binary; 2 = string; others = reserved
 } acon_get_report_req_t;
 
 typedef struct {
     acon_message_hdr_t header;   // command = 1
-    int rtmr_count;
-    td_report_t report;
-    int quote_offset;
-    int attestation_json_offset;
+    int32_t rtmr_count;
+    int32_t attestation_json_offset;
+    int32_t data_offset;
 } acon_get_report_rsp_t;
 
 typedef struct {
     acon_message_hdr_t header;  // command = 2
-    int data_type;              // Same definition as acon_get_report_req_t.data_type
+    int32_t attest_data_type;   // Same definition as acon_get_report_req_t.attest_data_type
 } acon_set_attestation_data_req_t;
 
 typedef struct {
